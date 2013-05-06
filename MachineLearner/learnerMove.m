68ddb707e3ac303ba23510a0c3b17d58ec85e9ca
Eric <epalace510@gmail.com>
2013-05-06 17:49:58 -0400

    Added logic for learner. Not working yet, but the basics are there. Still need to check for winning and write to the file at the end.

--- MachineLearner/learnerMove.m
+++ MachineLearner/learnerMove.m
@@ -1,6 +1,7 @@
 function [ output_args ] = learnerMove( input_args )
 global moveSuccess;
 global board;
+global movesTaken;
 %LEARNERMOVE Summary of this function goes here
 %   Detailed explanation goes here
 
@@ -10,10 +11,53 @@ global board;
 % Next, read the current board state and determine the 7 possible next
 % moves it can make. Check to see the success of those moves previously.
 % Take the highest scoring one and execute it.
+%array(column,row)
+% state=cat(2,board(1,:),board(2,:),board(3,:),board(4,:),board(5,:),board(6,:),board(7,:));
+% for l=1:size(tempBoard,1),
+%     if(l>0)
+%         if tempBoard(l,n)~=0
+%             l=l-1;
+%             break;
+%         end
+%     end
+% end
+score=0;
+for i=1:7
+    tempBoard=board;
+    for l=1:size(tempBoard,1),
+        if(l>0)
+            if tempBoard(l,i)~=0
+                l=l-1;
+                break;
+            end
+        end
+    end
+    %Set the next possible move
+    tempBoard(l,i)=2;
+    %Reduce it to a 1 dimensional integer
+    state=cat(2,tempBoard(1,:),tempBoard(2,:),tempBoard(3,:),tempBoard(4,:),tempBoard(5,:),tempBoard(6,:),tempBoard(7,:));
+    %Find the score of the move.
+    index=find(moveSuccess==state);
+    if(~isempty(index))
+        %Calculate the percentage won.
+        tscore=moveSuccess(index,2)/moveSuccess(index,3);
+    else
+        %Create an entry for this state since we've never visited it
+        %before. Give it 1/7 times won.
+        tscore=1/7;
+        newEntry=[state,1,7];
+        moveSuccess=cat(1,moveSuccess,newEntry);
+    end
+    if tscore>score
+        nextBoard=tempBoard;
+    end
+end
 
 % Add the chosen move to the list of moves taken this game (need global var
 % for this)
-
+board=nextBoard;
+state=cat(2,nextBoard(1,:),nextBoard(2,:),nextBoard(3,:),nextBoard(4,:),nextBoard(5,:),nextBoard(6,:),nextBoard(7,:));
+movesTaken=cat(1,movesTaken,state);
 
 end
 
